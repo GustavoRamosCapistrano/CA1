4858823ca7d7be7e478cb893318e974d955cf0ef
GustavoRamosCapistrano <sba23064@student.cct.ie>
2024-01-15 15:47:08 +0000

    adding the reader

--- src/ca1/CA1.java
+++ src/ca1/CA1.java
@@ -4,9 +4,11 @@
  */
 package ca1;
 
+import java.util.Scanner;
+import java.io.File;
 /**
  *
- * @author grc29
+ * @author Gustavo Capistrano
  */
 public class CA1 {
 
@@ -14,7 +16,25 @@ public class CA1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!);
+        
+        try{
+            Scanner sc = new Scanner(new File("students.txt"));
+            
+            String line1 = sc.nextLine();
+            String line2 = sc.nextLine();
+            String line3 = sc.nextLine();
+            
+            System.out.println("Line 1: " + line1);
+            System.out.println("Line 2: " + line2);
+            System.out.println("Line 3: " + line3);
+            
+            
+            sc.close();
+            } catch (Exception e) {
+            System.out.println(e);
+        }
+        }
+        
     }
     
-}
+
