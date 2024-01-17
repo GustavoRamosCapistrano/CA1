7cb53a1f150c93a2eaaf063eeed868694be95a7d
GustavoRamosCapistrano <sba23064@student.cct.ie>
2024-01-17 21:21:29 +0000

    adding checks

--- src/ca1/CA1.java
+++ src/ca1/CA1.java
@@ -3,6 +3,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package ca1;
+
+import java.io.FileReader;
+import java.util.Scanner;
+
 /**
  *
  * @author Gustavo Capistrano
@@ -13,7 +17,25 @@ public class CA1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        String students = "students.txt";
+         try{
+            Scanner sc = new Scanner(new FileReader(students));
+            
+            String line1 = sc.nextLine();
+            String line2 = sc.nextLine();
+            String line3 = sc.nextLine();
+            
+            
+            System.out.println("Line 1: " + line1);
+            System.out.println("Line 2: " + line2);
+            System.out.println("Line 3: " + line3);
+            
+            
+            sc.close();
+           
+            } catch (Exception e) {
+            System.out.println(e);
+            }
         }
         
     }
--- src/ca1/actions.java
+++ src/ca1/actions.java
@@ -5,6 +5,7 @@
 package ca1;
 
 import java.io.File;
+import java.io.FileReader;
 import java.util.Scanner;
 
 /**
@@ -12,26 +13,5 @@ import java.util.Scanner;
  * @author grc29
  */
 public class actions {
-    
-        String students = "students.txt";
-        
-        public void reader() {
-        try{
-            Scanner sc = new Scanner(new File(students));
-            
-            String line1 = sc.nextLine();
-            String line2 = sc.nextLine();
-            String line3 = sc.nextLine();
-            
-            
-            System.out.println("Line 1: " + line1);
-            System.out.println("Line 2: " + line2);
-            System.out.println("Line 3: " + line3);
-            
-            
-            sc.close();
-            } catch (Exception e) {
-            System.out.println(e);
-            }
-    }
+
 }
