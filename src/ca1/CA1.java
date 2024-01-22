fb7473abf8888aff38e13f1399aa6cb5a23e19bf
GustavoRamosCapistrano <sba23064@student.cct.ie>
2024-01-22 10:49:30 +0000

    Add work load and fixing checkStudentNum

--- src/ca1/CA1.java
+++ src/ca1/CA1.java
@@ -28,40 +28,61 @@ public class CA1 {
 
     }
 
-    public static boolean checkstudentNum(String studentNum) {
+    public static boolean checkStudentNum(String studentNum) {
         if (studentNum.length() >= 6) {
-            String firstTwoChars = studentNum.substring(0, 2);
+            
+        }else{
+            System.out.println("Student ID: length need to be 6.");
 
             try {
+                String firstTwoChars = studentNum.substring(0, 2);
                 int firstTwoDigits = Integer.parseInt(firstTwoChars);
                 if (firstTwoDigits > 20) {
+                    
                     char thirdChar = studentNum.charAt(2);
                     char forthChar = studentNum.charAt(3);
 
                     if (Character.isLetter(thirdChar) && Character.isLetter(forthChar)) {
-                        int currentIndex = 4;
                         
-                        char fifthChar = studentNum.charAt(currentIndex);
+                        
+                        char fifthChar = studentNum.charAt(4);
                         if (Character.isLetter(fifthChar)) {
-                            currentIndex++;
-                            while (currentIndex < studentNum.length()) {
-                                char currentChar = studentNum.charAt(currentIndex);
-                                if (!Character.isDigit(currentChar)) {
-                                    return false;
-                                }
-                                currentIndex++;
-                            }
-                            int remainingDigits = Integer.parseInt(studentNum.substring(4, currentIndex));
+                            int remainingDigits = Integer.parseInt(studentNum.substring(5));
                             return remainingDigits >= 1 && remainingDigits <= 0200;
+                        }else if(Character.isDigit(fifthChar)){
+                            int remainingDigits = Integer.parseInt(studentNum.substring(4));
+                            return remainingDigits >= 1 && remainingDigits <= 0200;
+                           
+                        }else{
+                            System.out.println("Invalid student ID: first and second must be a number and bigger than 20.");
+                        
                         }
+                        
+                    }else{
+                        System.out.println("Invalid student ID: 3th and 4th character must be letter.");
                     }
+                }else{
+                    System.out.println("Invalid student ID: 5th character mus be letter or number.");
                 }
-
             } catch (Exception e) {
+                System.out.println(e);
             }
 
         }
         return false;
+    }
+        public static String workload(int numClasses) {
+            String workload;
+            if (numClasses >=6){
+                workload = "full Time";
+            } else if(numClasses >=3 && numClasses < 5){
+                workload = "Part Time";
+            }else if (numClasses == 2){
+                workload = "Light";
+            }else if (numClasses == 1){
+                workload = "Very Light";
+            }
+           return workload;     
     }
 
     public static void main(String[] args) {
@@ -73,19 +94,18 @@ public class CA1 {
             String name = sc.nextLine();
             String numClasses = sc.nextLine();
             String studentNumString = sc.nextLine();
-
+            try{
+            int studentNum = Integer.parseInt(studentNumString);
+            
             System.out.println("Line 1: " + name);
             System.out.println("Line 2: " + numClasses);
             System.out.println("Line 3: " + studentNumString);
-            int studentNum = Integer.parseInt(studentNumString);
-            
-            if (checkNumClasses(studentNum)) {
-                System.out.println("valid Student Number.");
-            } else {
-                System.out.println("Invalid student number");
+            }catch(Exception e){
+                System.out.println(e);
             }
-            sc.close();
-
+            
+         
+sc.close();
         } catch (Exception e) {
             System.out.println(e);
         }
